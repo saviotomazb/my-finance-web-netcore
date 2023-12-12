21173c8e4cb2681e65face3a821a28e136f22ac7
Savio <saviotomaz_@hotmail.com>
2023-12-12 20:16:12 -0300

    Initial Commit

---
+++ my-finance-web-netcore/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace my_finance_web_netcore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
